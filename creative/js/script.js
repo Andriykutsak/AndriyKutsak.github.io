4566f0ab8ffb29792e93633f0c9a1349fd0d7481
AndriyKutsak <andriykutsak.ak@gmail.com>
2021-03-05 21:29:16 +0300

    bugs in creative project

--- creative/js/script.js
+++ creative/js/script.js
@@ -1,6 +1,6 @@
 (function(){
 	let sq=document.querySelectorAll(".rot-block");
-	window.addEventListener('resize',function(e){
+	window.addEventListener('load',function(e){
 			function setHeight(){
 		sq.forEach(function (el) {
 			el.style.height=el.clientWidth+"px";
